f079cb98713cd0f612a0413b64a40da8a5d2a6ca
zzY0123 <932528463@qq.com>
2021-01-25 23:20:57 +0800

    feat: add router

--- xueqiuWeb/src/app/app-routing.module.ts
+++ xueqiuWeb/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './view/index/index.component'
+import { ChooseComponent } from './view/choose/choose.component'
+import { NewstockComponent } from './view/newstock/newstock.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent
+  }, 
+  {
+    path: 'choose',
+    component: ChooseComponent
+  }, 
+  {
+    path: 'newstock',
+    component: NewstockComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
